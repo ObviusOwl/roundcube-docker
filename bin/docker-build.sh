a6c9ef1f99e54c2613d93fcbae0557254e50b971
Johannes ter Haak <jojo@terhaak.de>
2020-06-23 10:00:36 +0200

    roundcube 1.4.6

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -4,14 +4,16 @@ set -e
 # roundcube version 
 # use the git tag from
 # https://github.com/roundcube/roundcubemail/releases
-RC_VERSION="1.4.3"
+RC_VERSION="1.4.6"
 
 # image revision for multiple builds per day
-REV="2"
+REV="1"
 
 IMG_TAG="$RC_VERSION-`date +%Y%m%d`-$REV"
 RC_IMG="reg.lan.terhaak.de/jojo/roundcube:$IMG_TAG"
+DH_IMG="obviusowl/roundcube:$IMG_TAG"
 
-sudo docker build -t "$RC_IMG" \
+sudo docker build -t "$RC_IMG" -t "$DH_IMG" \
   --build-arg RC_VERSION="$RC_VERSION" \
   ./docker/roundcube
+
